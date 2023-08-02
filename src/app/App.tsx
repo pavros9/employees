e84e0575751277272125d1c4be6608bacb744253
pavro_roman <roman.pavlov.1996@bk.ru>
2023-08-02 16:42:10 +0300

    Added Navbar widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import './styles/index.scss';
 import './styles/tailwind.css';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     return (
         <>
+            <Navbar />
             <AppRouter />
         </>
     );
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+export const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(true);
+
+    const toggleButtonMenu = () => {
+        setOpenMenu((prev) => !prev);
+    };
+
+    return (
+        <nav className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <button
+                            type="button"
+                            onClick={toggleButtonMenu}
+                            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            aria-controls="mobile-menu"
+                            aria-expanded="false"
+                        >
+                            <span className="absolute -inset-0.5"></span>
+                            <span className="sr-only">Открыть меню</span>
+
+                            <svg
+                                className="block h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                                />
+                            </svg>
+                            <svg
+                                className="hidden h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                <a
+                                    href="#"
+                                    className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                                >
+                                    Работники
+                                </a>
+                                <a
+                                    href="#"
+                                    className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                                    aria-current="page"
+                                >
+                                    Настройка
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div
+                className={classNames(
+                    'sm:hidden transition-height duration-500 ease-in-out overflow-hidden',
+                    {},
+                    [openMenu ? 'h-32' : 'h-0'],
+                )}
+                id="mobile-menu"
+            >
+                <div className="space-y-1 px-2 pb-3 pt-2">
+                    <a
+                        href="#"
+                        className="bg-gray-900 text-white block  rounded-md px-3 py-2 text-sm font-medium"
+                        aria-current="page"
+                    >
+                        Работники
+                    </a>
+                    <a
+                        href="#"
+                        className="bg-gray-900 text-white block  rounded-md px-3 py-2 text-sm font-medium"
+                        aria-current="page"
+                    >
+                        Настройка
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
