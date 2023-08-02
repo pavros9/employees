3b2e3f12f5eac693e6753268d2d7f7c6ebc03cdd
pavro_roman <roman.pavlov.1996@bk.ru>
2023-08-02 16:40:23 +0300

    Added classNames lib

---
+++ src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,17 @@
+type Mods = Record<string, string | boolean | undefined>;
+
+export function classNames(
+    cls: string,
+    mods: Mods = {},
+    additional: Array<string | undefined> = [],
+): string {
+    return [
+        cls,
+        ...additional.filter(Boolean),
+        ...Object.entries(mods)
+            .filter(([className, value]) => Boolean(value))
+            .map(([className, value]) => className),
+    ]
+        .join(' ')
+        .trim();
+}
