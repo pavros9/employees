f6e8a130eaf51d0baa79cc27b391f1d100d939cc
pavro_roman <roman.pavlov.1996@bk.ru>
2023-09-13 16:43:25 +0300

    Configured build for production(env)

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,9 +3,12 @@ import webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import * as dotenv from 'dotenv'
 
 export function buildPlugins (options: BuildOptions): webpack.WebpackPluginInstance[] {
     const { paths, isDev, analyze, apiUrl } = options
+
+    const env =  dotenv.config({path: `.env.${isDev ? 'development' : 'production'}`})
     return [
         new HtmlWebpackPlugin({
             template: paths.html
@@ -17,7 +20,8 @@ export function buildPlugins (options: BuildOptions): webpack.WebpackPluginInsta
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-            __API__: JSON.stringify(apiUrl)
+            __API__: JSON.stringify(apiUrl),
+            'process.env': JSON.stringify(env.parsed)
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: analyze ? 'server' : 'disabled'
--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const $api = axios.create({
-    baseURL: __API__,
+    baseURL: `${process.env.API_URL}:${process.env.PORT}`,
 });
